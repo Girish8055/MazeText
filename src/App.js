d86914ea717d8f8cce0b7b0f2a7b263e5262194d
Girish <109870375+Girish8055@users.noreply.github.com>
2023-11-28 19:50:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState("light"); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+    }
+  };
+
+  return (
+    <>
+      <Router>
+        <Navbar
+          title="MazeText"
+          mode={mode}
+          toggleMode={toggleMode}
+          key={new Date()}
+        />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            {/* <Route path="/about" element={<About mode={mode} />} /> */}
+            <Route
+              path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Try MazeText - for Analyze the Words"
+                  mode={mode}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
